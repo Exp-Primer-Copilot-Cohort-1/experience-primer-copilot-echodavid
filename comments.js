787d1d3ba431673884e248de1a64c0922333f20c
David Espinosa <138745702+echodavid@users.noreply.github.com>
2023-09-19 23:59:34 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,25 @@
+// Create a web server
+// 1. send back json and handle 404
+
+const http = require('http')
+const url = require('url')
+
+const server = http.createServer((req, res) => {
+    const pathName = req.url
+
+    if (pathName === '/' || pathName === '/overview') {
+        res.end('This is the overview')
+    } else if (pathName === '/product') {
+        res.end('This is the product')
+    } else {
+        res.writeHead(404, {
+            'Content-type': 'text/html',
+            'my-own-header': 'hello-world'
+        })
+        res.end('<h1>Page not found!</h1>')
+    }
+})
+
+server.listen(8000, '
+
+
