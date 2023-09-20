97da9b710898a7cd235d54212fb83ba3fcd1ff5b
David Espinosa <138745702+echodavid@users.noreply.github.com>
2023-09-20 00:14:25 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,25 +1,21 @@
-// Create a web server
-// 1. send back json and handle 404
+// Create a web server that can respond to requests for comments
+// from a specific post
+// 1. Create an express web server
+// 2. Create an endpoint for fetching a list of comments for a post
+// 3. Setup a "start" script and "start" it
 
-const http = require('http')
-const url = require('url')
+const express = require('express');
+const axios = require('axios');
 
-const server = http.createServer((req, res) => {
-    const pathName = req.url
+const app = express();
 
-    if (pathName === '/' || pathName === '/overview') {
-        res.end('This is the overview')
-    } else if (pathName === '/product') {
-        res.end('This is the product')
-    } else {
-        res.writeHead(404, {
-            'Content-type': 'text/html',
-            'my-own-header': 'hello-world'
-        })
-        res.end('<h1>Page not found!</h1>')
-    }
-})
+app.get('/comments', async (req, res) => {
+  const { data } = await axios.get(
+    'https://jsonplaceholder.typicode.com/comments'
+  );
+  res.send(data);
+});
 
-server.listen(8000, 'git')
-
-
+app.listen(4001, () => {
+  console.log('Listening on 4001');
+});
